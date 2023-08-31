b7d7356d9a02be3fd32ece3df0c33be32edf311f
Javier Horacio Fuchs <javier.horacio.fuchs@icloud.com>
2023-08-31 20:50:19 -0300

    Handy Lib Net 7

---
+++ HandyLibNet7/Helper.cs
@@ -0,0 +1,12 @@
+﻿namespace HandyLibNet7;
+
+// All the code in this file is included in all platforms.
+public static class Helper
+{
+
+    public static int Diga33()
+    {
+        return 33;
+    }
+}
+
---
+++ HandyLibNet7/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace HandyLibNet7;
+
+// All the code in this file is only included on Mac Catalyst.
+public class PlatformClass1
+{
+}
+
